402738a54798c6788f890e45dfa0c9241b3997ad
cfanr <navyifanr@gmail.com>
2020-07-19 23:32:37 +0800

    week 06 homework

---
+++ Week06/Exercise/[322]零钱兑换.java
@@ -0,0 +1,75 @@
+//给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回
+// -1。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: coins = [1, 2, 5], amount = 11
+//输出: 3 
+//解释: 11 = 5 + 5 + 1 
+//
+// 示例 2: 
+//
+// 输入: coins = [2], amount = 3
+//输出: -1 
+//
+// 
+//
+// 说明: 
+//你可以认为每种硬币的数量是无限的。 
+// Related Topics 动态规划
+
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    //方法 1：递归+dp
+    public int coinChange(int[] coins, int amount) {
+        if (amount < 0) {
+            return -1;
+        }
+        Arrays.sort(coins); //先排序
+        int[] memo = new int[amount + 1];
+        return dp(coins, amount, memo);
+    }
+
+    public int dp(int[] coins, int amount, int[] memo) {
+        if (amount < 0) {
+            return -1;
+        }
+        if (amount == 0) {
+            return 0;
+        }
+        if (memo[amount] != 0) {
+            return memo[amount];
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = coins.length - 1; i >= 0; i--) {
+            int count = dp(coins, amount - coins[i], memo);
+            if (count == -1) continue;
+            min = Math.min(min, count + 1);  //注意加一
+        }
+        memo[amount] = min == Integer.MAX_VALUE ? -1 : min;
+        return memo[amount];
+    }
+
+    //方法 2：DP 方程： f(n) = min{f(n - ai) + 1}
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);  //注意，取比 amount 大 1
+        dp[0] = 0;    //要将 0 索引赋值
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i >= coins[j]) { //要凑的钱 i，比币种值大，才有必要对比
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
